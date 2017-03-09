4a189480bf5f1d2b1c9223f2375fc384409b6347
Anastasia <ana.dzmv@gmail.com>
2017-03-09 22:01:49 +0200

    NG: adding primary components

--- Scheduler2/src/app/app.module.ts
+++ Scheduler2/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+import { ScheduleViewComponent } from './schedule-view/schedule-view.component';
+import { NewSubjectComponent } from './new-subject/new-subject.component';
+import { EditSubjectComponent } from './edit-subject/edit-subject.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    ProfileViewComponent,
+    ScheduleViewComponent,
+    NewSubjectComponent,
+    EditSubjectComponent,
+    NewTaskComponent,
+    EditTaskComponent
   ],
   imports: [
     BrowserModule,
